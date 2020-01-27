1de860e8848f421a95f4e2e00b9d266132e47da0
knowomiejski <j.nowomiejski1998@gmail.com>
2020-01-27 13:31:32 +0100

    first commit ccbot - dotnet :bear:

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+
+
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post ([FromBody] string value)
+        {
+            
+        }
+
+
+    }
+}
