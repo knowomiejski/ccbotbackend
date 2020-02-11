c820528692769c5f2271f39c74d8b5c5c79acad1
Kuba Nowomiejski <j.nowomiejski1998@gmail.com>
2020-02-11 19:16:41 +0100

    :bear: Testing out how background services work

---
+++ API/BackgroundTasks/QuestionsBotService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace API.Services
+{
+    public class QuestionsBotService : BackgroundService
+    {
+        private IQuestionsBotQueue _queue;
+        
+        public QuestionsBotService(IQuestionsBotQueue queue)
+        {
+            
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var task = await _queue.PopQueue(stoppingToken);
+
+                await task(stoppingToken); 
+            }
+        }
+    }
+}
---
+++ API/BackgroundTasks/QuestionsBotTasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public interface IQuestionsBotQueue
+    {
+        void QueueTask(Func<CancellationToken, Task> task);
+        Task<Func<CancellationToken, Task>> PopQueue(CancellationToken cancellationToken);
+    }
+
+    public class QuestionsBotQueue : IQuestionsBotQueue
+    {
+        private ConcurrentQueue<Func<CancellationToken, Task>> _tasks;
+        private SemaphoreSlim _signal;
+        
+        public QuestionsBotQueue()
+        {
+            _tasks = new ConcurrentQueue<Func<CancellationToken, Task>>();
+            _signal = new SemaphoreSlim(0);
+        }
+
+        public void QueueTask(Func<CancellationToken, Task> task)
+        {
+            _tasks.Enqueue(task);
+            _signal.Release();
+        }
+
+        public async Task<Func<CancellationToken, Task>> PopQueue(CancellationToken cancellationToken)
+        {
+            await _signal.WaitAsync(cancellationToken);
+            _tasks.TryDequeue(out var task);
+            return task;
+        }
+    }
+}
